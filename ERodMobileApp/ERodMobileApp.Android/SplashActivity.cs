27bd5633dc5bf12343927bb8ad254dd3c73c5ad9
pallavgithub <pallav.algoworks@gmail.com>
2020-12-02 23:01:48 +0530

    Splash activity in android

--- ERodMobileApp/ERodMobileApp.Android/SplashActivity.cs
+++ ERodMobileApp/ERodMobileApp.Android/SplashActivity.cs
@@ -1,6 +1,10 @@
 using Android.App;
 using Android.Content;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
 
 namespace ERodMobileApp.Droid
 {
@@ -13,7 +17,36 @@ namespace ERodMobileApp.Droid
         protected override void OnResume()
         {
             base.OnResume();
-            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            SetContentView(Resource.Layout.SplashLayout);
+
+            VideoView videoView = (VideoView)FindViewById<VideoView>(Resource.Id.splashVideo);
+
+            var isLongVideoRun = Preferences.Get("IsLongVideoRun", false);
+            String uriPath;
+            if (isLongVideoRun)
+            {
+                uriPath = "android.resource://" + Android.App.Application.Context.PackageName + "/" + Resource.Raw.an1;
+            }
+            else
+            {
+                uriPath = "android.resource://" + Android.App.Application.Context.PackageName + "/" + Resource.Raw.an2;
+                Preferences.Set("IsLongVideoRun", true);
+            }
+
+            Android.Net.Uri videoPlay = Android.Net.Uri.Parse(uriPath);
+            videoView.SetVideoURI(videoPlay);
+            videoView.Visibility = ViewStates.Visible;
+
+            videoView.Prepared += delegate {
+                videoView.Start();
+            };
+
+            videoView.Completion += delegate {
+                //Intent intent = new Intent(this, typeof(MainActivity));
+                //StartActivity(intent);
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            };
         }
     }
 }
