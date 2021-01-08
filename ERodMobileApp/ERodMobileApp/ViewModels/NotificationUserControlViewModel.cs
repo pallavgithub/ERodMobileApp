e9b4c2efd069d97cb97b5ec9ae5b367958f2f30f
Priyanka Singh <priyanka.singh25@hotmail.com>
2021-01-08 17:23:55 +0530

    NotificationPage UI

---
+++ ERodMobileApp/ERodMobileApp/ViewModels/NotificationUserControlViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Navigation;
+using System.Collections.ObjectModel;
+
+namespace ERodMobileApp.ViewModels
+{
+    public class NotificationUserControlViewModel:ViewModelBase
+    {
+        public ObservableCollection<Notification> _notificationList;
+        public ObservableCollection<Notification> NotificationList
+        {
+            get => _notificationList;
+            set => SetProperty(ref _notificationList, value);
+        }
+        public NotificationUserControlViewModel(INavigationService navigationService):base(navigationService)
+        {
+            GetAllNotifications();
+        }
+        public void GetAllNotifications()
+        {
+            NotificationList = new ObservableCollection<Notification>();
+            NotificationList.Add(new Notification {Date="11/2/20",Name="New Order Received",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Arriving Tomorrow",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Order Confirmed",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Order Processing",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Order Shipped",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Order Delivered",Id= "EGSPUGE-234-K694G" });
+            NotificationList.Add(new Notification {Date="11/2/20",Name="Signature Required",Id= "EGSPUGE-234-K694G" });
+        }
+    }
+    public class Notification
+    {
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
